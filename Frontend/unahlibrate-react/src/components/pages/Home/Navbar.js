62b13393ff340bd5d26af94fafe7cc5834fc4a64
katherineES98 <katsantos1997@gmail.com>
2021-07-05 21:53:56 -0600

    feat: 🎸 Estado de opciones del nav

--- Frontend/unahlibrate-react/src/components/pages/Home/Navbar.js
+++ Frontend/unahlibrate-react/src/components/pages/Home/Navbar.js
@@ -4,10 +4,12 @@ import { Menu, Dropdown, Button as Buttonand, Space } from "antd";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
+
+        /**Estado de nav , opciones */
+  const [isSigned, setIsSigned] = useState(false);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
@@ -29,10 +31,10 @@ function Navbar() {
   const menu = (
     <Menu>
       <Menu.Item>
-        <Link to="/formclv">Actualizar Contrasena</Link>
+        <Link to="/formclv">Actualizar Contraseña</Link>
       </Menu.Item>
       <Menu.Item>
-        <Link to="/">Cerrar Sesion</Link>
+        <Link to="/">Cerrar Sesión </Link>
       </Menu.Item>
     </Menu>
   );
@@ -47,58 +49,78 @@ function Navbar() {
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
-                Acerca de Nosotros
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/registro"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Registrarse
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/actualizarPerfil"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Actualizar Perfil
-              </Link>
-            </li>
 
-            <li>
-              <Link
-                to="/login"
-                className="nav-links-mobile"
-                onClick={closeMobileMenu}
-              >
-                Login
-              </Link>
-            </li>
+          {!isSigned ? (
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/about"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Acerca de Nosotros
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/registro"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Registrarse
+                </Link>
+              </li>
 
-            <li>
-            <Dropdown  className="nav-links-mobile btn-opciones" overlay={menu} placement="bottomLeft">
-            <Buttonand>Opciones</Buttonand>
-          </Dropdown>
-          </li>
-          
-          </ul>
-          {button && <Button buttonStyle="btn--outline">Login</Button>}
-          
-          {button && (
-            <Dropdown classNamee= "btn--outline " overlay={menu} placement="bottomLeft">
-              <Buttonand className = "btn-options-full">Opciones</Buttonand>
+              <li>
+                <Link
+                  to="/login"
+                  className="nav-links-mobile"
+                  onClick={closeMobileMenu}
+                >
+                  Login
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link
+                  to="/actualizarPerfil"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Actualizar Perfil
+                </Link>
+              </li>
+
+              <li>
+                <Dropdown
+                  className="nav-links-mobile btn-opciones"
+                  overlay={menu}
+                  placement="bottomLeft"
+                >
+                  <Buttonand>Opciones</Buttonand>
+                </Dropdown>
+              </li>
+            </ul>
+          )}
+
+          {button && !isSigned && (
+            <Button buttonStyle="btn--outline">Login</Button>
+          )}
+
+          {button && isSigned && (
+            <Dropdown
+              classNamee="btn--outline "
+              overlay={menu}
+              placement="bottomLeft"
+            >
+              <Buttonand className="btn-options-full">Opciones</Buttonand>
             </Dropdown>
           )}
         </div>
