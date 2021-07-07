6a89148fc20c9907cbb6c7b15756e86deeddf29a
Lisan274 <lisan3@hotmail.com>
2021-07-07 16:04:46 -0600

    Agregando descripcion de las cards

--- Frontend/unahlibrate-react/src/components/pages/cards/Cards.js
+++ Frontend/unahlibrate-react/src/components/pages/cards/Cards.js
@@ -11,29 +11,29 @@ function Cards() {
           <ul className='cards__items'>
           <CardItem
               src='images/8.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'
+              text='Expediciones matematicas es una amena seleccion de
+              mas de quinientos problemas de diferentes epocas y civilizaciones...'
               label='Matematicas'
               path='/sign-up'
             />
            <CardItem
               src='images/3.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'
+              text='La literatura nos ayuda a vivir y nos permite responder mejor
+              a nuestra vocacion de seres humanos. Estas dos ideas sencillas...'
               label='Literatura'
               path='/services'
             />
             <CardItem
               src='images/1.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'
+              text='Quiénes son los millennials colombianos presenta los resultados de una 
+              investigación que tuvo como objetivo conocer...'
               label='Cultural'
               path='/services'
             />
             <CardItem
               src='images/2.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'
+              text='​Indaga en la relación entre emociones, 
+              expresiones faciales y la cultura en ese tipo de encuentros...'
               label='Cultural'
               path='/services'
             />
@@ -41,29 +41,29 @@ function Cards() {
           <ul className='cards__items'>
             <CardItem
               src='images/9.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'
+              text='Para los que nacimos en la segunda mitad del siglo XX 
+              y algunos en este siglo, el nombre Baldor es sinónimo de conocimiento, educación, calidad...'
               label='Matematicas'
               path='/services'
             />
             <CardItem
               src='images/4.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'
+              text='El presente libro hace parte de una gran publicación sobre Minería y desarrollo 
+              elaborada por un grupo interdisciplinario de más de noventa investigadores...'
               label='Historia'
               path='/products'
             />
             <CardItem
               src='images/6.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'
+              text='La explotación indiscriminada de los recursos ambientales nos ha llevado 
+              a pensar que somos externos al medio ambiente...'
               label='Ambiental'
               path='/sign-up'
             />
             <CardItem
               src='images/7.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'
+              text='Libro para la formacion integral de ambientes educativos sin importar
+              el grado educativo llevado...'
               label='Cultural'
               path='/sign-up'
             />
