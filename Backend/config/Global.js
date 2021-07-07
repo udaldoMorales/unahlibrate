214a2f12d07cef47570d222e152ab71c16b92cdf
larizasandoval <-list>
2021-07-07 00:03:35 -0600

    duracion de la sesion

--- Backend/config/Global.js
+++ Backend/config/Global.js
@@ -1,6 +1,6 @@
 module.exports = Global = {
 	urlApi: 'http://localhost:3900/api/',
 	//Cosas del token.
-	tokenExpires: '1m',
-	refreshTokenExpires: 120 //Segundos. Va en números, por el modelo de refreshtoken.
+	tokenExpires: '5m',
+	refreshTokenExpires: 600 //Segundos. Va en números, por el modelo de refreshtoken.
 }
